e02810be14da7894c0cdb4e5afe7ba28664a5b9d
ali <ali.mrd318@gmail.com>
2022-10-21 18:15:09 +0330

    Refactor : change name

--- trade_handler/envs.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-DEBUG_MODE = getenv("DEBUG_MODE", "true")
-PROXY_IP = getenv("PROXY_IP", None)
-PROXY_PORT = getenv("PROXY_PORT", None)
-
-TELEGRAM_CHAT_ID = getenv("TELEGRAM_CHAT_ID", None)
-TELEGRAM_BOT_TOKEN = getenv("TELEGRAM_BOT_TOKEN", None)
